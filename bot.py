6a102bac502ab6adfc9f5e452c6d27cbd637ada3
rkg26176 <rkg26176@gmail.com>
2026-05-21 10:16:17 +0100

    Added bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+import asyncio
+
+TOKEN = "8903550566:AAFFjzsFE8oh1AO_AHXpiLnb1ArCk7PoeTI"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer("🔥 GBX Coupon Bot Online Hai")
+
+async def main():
+    await dp.start_polling(bot)
+
+asyncio.run(main())
